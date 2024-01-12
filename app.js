ce7da275eeaece46eebf6511b9a14886cbd5b5b5
karthik Raj <61104188+karthikdedsec@users.noreply.github.com>
2024-01-12 15:26:25 +0530

    configure environment

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 //1) middle wares
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('hello from middleware');
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 //4)start server
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`APP running on port ${3000}....`);
 });
